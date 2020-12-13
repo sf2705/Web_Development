733290173309d597894b7bda4d339761ac014b5d
Siqi Feng <36309789+sf2705@users.noreply.github.com>
2020-12-13 09:32:32 -0500

    feat: add trace for plot2 in js file

--- javascript_files/plot2.js
+++ javascript_files/plot2.js
@@ -10,10 +10,14 @@ var country_name_china = 'China';
 
 var trace1 = {
 /* TODO: Use the country name and arable land data to make a bar chart */
+  x: [country_name_brazil, country_name_germany, country_name_china],
+  y: [arable_land_brazil[0], arable_land_germany[0], arable_land_china[0]],
+  type: 'bar'
 };
 
 var layout = {
  /* TODO: include a title for the chart */
+  title: 'Percent of Land Used by Country <br> in Year 2015'
 };
 
 var data = [trace1];
